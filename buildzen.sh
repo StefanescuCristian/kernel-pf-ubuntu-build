cafab07f7fe78ec19a3324c053c3b1349f65d8c8
Stefanescu Cristian <howerkraft@gmail.com>
2015-06-28 13:17:45 +0300

    use optimizations
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildzen.sh
+++ buildzen.sh
@@ -1,4 +1,5 @@
 cp /boot/config-`uname -r` .config
 make oldconfig
 #make menuconfig
+sed -i 's/-O2/-O3 -fgraphite -fgraphite-identity -floop-parallelize-all -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block -ftree-vectorize/g' Makefile
 time make-kpkg -j4 --initrd --rootcmd fakeroot kernel_image kernel_headers modules_image
