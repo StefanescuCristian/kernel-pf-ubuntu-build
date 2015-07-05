ebe70b936428bd381a04a391cf3f981476b97431
Stefanescu Cristian <howerkraft@gmail.com>
2015-07-05 13:02:10 +0300

    unsafe optimizations
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildzen.sh
+++ buildzen.sh
@@ -2,5 +2,5 @@ make mrproper
 cp /boot/config-`uname -r` .config
 make oldconfig
 #make menuconfig
-sed -i 's/-O2/-O3 -fgraphite -fgraphite-identity -floop-parallelize-all -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block -ftree-vectorize/g' Makefile
+sed -i 's/-O2/-Ofast -fgraphite -fgraphite-identity -floop-parallelize-all -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block -ftree-vectorize -floop-nest-optimize -fgcse -fgcse-lm -fgcse-sm -fgcse-las -fgcse-after-reload -march=native/g' Makefile
 time make-kpkg -j4 --initrd --rootcmd fakeroot kernel_image kernel_headers modules_image
