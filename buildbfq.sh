e091e290b7d99d771af6653125640eb4c7e608bd
Stefanescu Cristian <howerkraft@gmail.com>
2015-10-28 23:18:18 +0200

    update buildscript
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildbfq.sh
+++ buildbfq.sh
@@ -10,11 +10,11 @@
 #kernel version base
 kernel="4.2"
 #kernel specific version
-patchkernel="4.2.4"
+patchkernel="4.2.5"
 #BFQ patch
 bfq="4.2.0-v7r9"
 #####################################
-
+distro="wily"
 ################################################################################
 ############DO NOT EDIT BELOW UNLESS YOU KNOW WHAT YOU'RE DOING#################
 ################################################################################
@@ -41,18 +41,18 @@ _printfs "downloading mainstream patches ..."
 _waitfor wget --no-check-certificate -N http://www.kernel.org/pub/linux/kernel/v4.x/patch-"${patchkernel}".gz
 [ ! -f patch-"${patchkernel}".gz ] && _die "couldn't get http://www.kernel.org/pub/linux/kernel/v4.x/patch-${patchkernel}.gz"
 fi;
-
+bfq_mirror="ftp://teambelgium.net/bfq/patches"
+bfq_main="http://algo.ing.unimo.it/people/paolo/disk_sched/patches"
 _printfl "downloading bfq patches ..."
-_waitfor wget -N "http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0001-block-cgroups-kconfig-build-bits-for-BFQ-${vbfq}-${kernel}.patch"
-[ ! -f "0001-block-cgroups-kconfig-build-bits-for-BFQ-${vbfq}-${kernel}.patch" ] && \
-    _die "couldn't get http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0001-block-cgroups-kconfig-build-bits-for-BFQ-${vbfq}-${kernel}.patch"
-_waitfor wget -N "http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0002-block-introduce-the-BFQ-${vbfq}-I-O-sched-for-${kernel}.patch"
-[ ! -f "0002-block-introduce-the-BFQ-${vbfq}-I-O-sched-for-${kernel}.patch" ] && \
-    _die "couldn't get http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0002-block-introduce-the-BFQ-${vbfq}-I-O-sched-for-${kernel}.patch"
-_waitfor wget -N "http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-${vbfq}-for-${kernel}.0.patch"
-[ ! -f "0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-${vbfq}-for-${kernel}.0.patch" ] && \
-    _die "couldn't get http://algo.ing.unimo.it/people/paolo/disk_sched/patches/${bfq}/0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-${vbfq}-for-${kernel}.0.patch"
+_waitfor wget -N "$bfq_mirror/${bfq}/0001-block-cgroups-kconfig-build-bits-for-BFQ-${vbfq}-${kernel}.patch"
+_waitfor wget -N "$bfq_mirror/${bfq}/0002-block-introduce-the-BFQ-${vbfq}-I-O-sched-for-${kernel}.patch"
+_waitfor wget -N "$bfq_mirror/${bfq}/0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-${vbfq}-for-${kernel}.0.patch"
 _waitfor wget -N "https://raw.githubusercontent.com/graysky2/kernel_gcc_patch/master/enable_additional_cpu_optimizations_for_gcc_v4.9%2B_kernel_v3.15%2B.patch"
+
+_printfl "downloading ubuntu patches"
+_waitfor wget -N "http://kernel.ubuntu.com/~kernel-ppa/mainline/v$patchkernel-$distro/0001-base-packaging.patch"
+_waitfor wget -N "http://kernel.ubuntu.com/~kernel-ppa/mainline/v$patchkernel-$distro/0002-debian-changelog.patch"
+_waitfor wget -N "http://kernel.ubuntu.com/~kernel-ppa/mainline/v$patchkernel-$distro/0003-configs-based-on-Ubuntu-4.2.0-18.22.patch"
 
 _printfl "Applying patches"
 _printfs "uncompresing kernel to ${tmp_path}/linux-${kernel}/ ..."
@@ -76,10 +76,12 @@ if [ "$patchkernel" != 0 ]; then _cmd     "patch -p1 < ../patch-${patchkernel}";
 _cmd     "patch -p1 < ../0001-block-cgroups-kconfig-build-bits-for-BFQ-${vbfq}-${kernel}.patch"
 _cmd     "patch -p1 < ../0002-block-introduce-the-BFQ-${vbfq}-I-O-sched-for-${kernel}.patch"
 _cmd     "patch -p1 < ../0003-block-bfq-add-Early-Queue-Merge-EQM-to-BFQ-${vbfq}-for-${kernel}.0.patch"
-_cmd	 "sed -i 's/-O2/-Ofast -fgraphite -fgraphite-identity -floop-parallelize-all -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block -ftree-vectorize -floop-nest-optimize -fgcse -fgcse-lm -fgcse-sm -fgcse-las -fgcse-after-reload -march=native/g' Makefile"
+_cmd	 "sed -i 's/-O2/-Ofast -fgraphite -fgraphite-identity -floop-parallelize-all -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block -ftree-vectorize -floop-nest-optimize -fgcse -fgcse-lm -fgcse-sm -fgcse-las -fgcse-after-reload -march=native -pipe/g' Makefile"
 _cmd	 "patch -p1 < ../enable_additional_cpu_optimizations_for_gcc_v4.9+_kernel_v3.15+.patch"
+_cmd	 "patch -p1 < ../0001-base-packaging.patch"
+_cmd	 "patch -p1 < ../0002-debian-changelog.patch"
+_cmd	 "patch -p1 < ../0003-configs-based-on-Ubuntu-4.2.0-18.22.patch"
 make mrproper
-cp /boot/config-4.2.3-040203-generic .config
 make oldconfig
 #make menuconfig
 time make-kpkg -j5 --initrd --rootcmd fakeroot kernel_image kernel_headers modules_image
